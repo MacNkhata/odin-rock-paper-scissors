2c264ad2704863e49f3675a9e5697e5ee2a5d5fd
Macdonald Nkhata <nkhatamac27@gmail.com>
2025-02-03 17:59:40 +0200

    feat generate random responses

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+const getComputerChoice = () => {
+  let choices = ["rock", "paper", "scissors"];
+  const choice = Math.floor(Math.random() * 3);
+
+  return choices[choice];
+};
+
+let choice = getComputerChoice();
+
+console.log(choice);
