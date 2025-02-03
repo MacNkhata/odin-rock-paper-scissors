afbd73383bb46b8da89647255bdd81e2d8d5855c
Macdonald Nkhata <nkhatamac27@gmail.com>
2025-02-03 18:03:30 +0200

    fix change function to use if statement

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const getComputerChoice = () => {
-  let choices = ["rock", "paper", "scissors"];
+// const getComputerChoice = () => {
+//   let choices = ["rock", "paper", "scissors"];
+//   const choice = Math.floor(Math.random() * 3);
+
+//   return choices[choice];
+// };
+
+function getComputerChoice() {
   const choice = Math.floor(Math.random() * 3);
 
-  return choices[choice];
-};
+  if (choice == 0) {
+    return "rock";
+  } else if (choice == 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
 
 let choice = getComputerChoice();
 
